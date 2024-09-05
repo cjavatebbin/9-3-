ead913b51e0e85b7087a96ef645678e5e7e1c4fa
cjavatebbin <thwu2021@gmail.com>
2024-09-04 17:23:20 -0700

    Update SecondsConverterMain.java

--- SecondsConverterMain.java
+++ SecondsConverterMain.java
@@ -9,6 +9,8 @@ public class SecondsConverterMain {
     public static void main(String[] args) {
         SecondsConverter converter = new SecondsConverter();
         System.out.println("50,391 seconds is equivalent to " + converter.secondsToHMS(50391));
-        // TODO: Write at least two more test cases here
+        System.out.println("12,345 seconds is equivalent to " + converter.secondsToHMS(12345));
+        System.out.println("362880 seconds is equivalent to " + converter.secondsToHMS(362880));
+        
     }
 }
